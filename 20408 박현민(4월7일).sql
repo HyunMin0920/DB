f61c28c927c41e416f35fa62e034e601b5f44c96
HyunMin0920 <rec080920@gmail.com>
2025-04-07 11:51:53 +0900

    Add files via upload

--- 20408 박현민(4월7일).sql
+++ 20408 박현민(4월7일).sql
@@ -10,6 +10,19 @@ select ename||' '' s sal is $'||sal from emp;
 --조건
 select empno,ename from emp where empno = 7900;
 select empno,ename, sal from emp where sal < 1000;
---산술연산자
+select ename,hiredate from emp where hiredate < = '81/12/25';
 select ename,sal,deptno from emp where deptno = 10;
 select ename,sal+100,deptno from emp where deptno = 10;
+select empno,ename,sal from emp where sal Between 2450 and 3000; 
+select empno,ename,sal from emp where sal >= 2450 and sal<=3000;
+select empno,ename,deptno from emp where deptno in (10,20);
+select empno,ename,deptno from emp where ename in ('KING','FORD');
+select empno,ename,sal from emp where sal LIKE'1%';
+select empno,ename,sal from emp where ename LIKE'A%';
+select empno,ename,hiredate from emp where hiredate LIKE'%80%';
+select empno,ename,sal from emp where ename LIKE'__AR%';
+select ename,hiredate,sal from emp where hiredate >'81-01-01'and sal>=1300;
+select ename,hiredate,sal from emp where hiredate >'82-01-01'or sal>=1300;
+select studno,name,deptno1,1 from student where deptno1 = 101
+Union
+select profno,name,deptno,2 from professor where deptno = 101;
